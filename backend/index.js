1c1e3175d5f9c01b41fa1a2542cb8c6fbcee5a6c
itzadmin001 <158562740+itzadmin001@users.noreply.github.com>
2025-08-26 12:12:50 +0530

    vercel.json add

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,10 @@ app.use(express.json())
 app.use(cookieParser())
 
 
+app.get("/", (req, res) => {
+    res.send("API is running")
+})
+
 app.use("/user", UserRouter)
 app.use("/prompt", PromptRouter)
 app.use("/dashboard", VerifyUser, UserRouter)
